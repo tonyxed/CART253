ece521d868612b3fb30652960a9798425809aafa
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-08 12:54:21 -0400

    Mouth of Alien

--- Activities/02-draw-an-alien/js/script.js
+++ Activities/02-draw-an-alien/js/script.js
@@ -10,6 +10,7 @@ Here is a description of this template p5 project.
 // setup()
 //
 // Description of setup() goes here.
+
 function setup() {
   // Canvas Dimensions
   createCanvas(500, 500);
@@ -18,23 +19,29 @@ function setup() {
 
   ellipseMode(CENTER);
   // Head of Alien with Dimensions and Color
-  stroke(53, 69, 64);
+  noStroke();
   fill(35, 135, 24);
   ellipse(255, 255, 255, 255);
   // Body of Alien
-  stroke(53, 69, 64);
+  noStroke();
   fill(35, 135, 24);
   ellipse(255, 500, 400, 300);
 
   // Left Eye of Alien
-  stroke(10, 0, 5);
+  noStroke();
   fill(12, 28, 23);
   ellipse(200, 215, 50, 120);
   // Right Eye of Alien
-  stroke(10, 0, 5);
+  noStroke();
   fill(12, 28, 23);
   ellipse(300, 215, 50, 120);
-  
+  // Mouth of Alien
+  rectMode(CENTER);
+  stroke(255, 0, 0);
+  strokeWeight(10);
+  fill(255, 0, 0);
+  rect(255, 320, 45, 10);
+
 }
 
 // draw()
