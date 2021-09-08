c08a079d40715e615e8f41f370a714a85b515d0c
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-08 11:57:04 -0400

    Comments

--- Experiments/template-p5-project/js/script.js
+++ Experiments/template-p5-project/js/script.js
@@ -11,26 +11,28 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
+  //Creates the createCanvas
   createCanvas(500, 500);
 
+  //Sets the Background Color
   background(100, 110, 111);
 
-  //noStroke();
+  //Sets the Actual Color
   stroke(255, 100, 2);
   fill(50, 80, 100, 120);
-
+  //Draws the Shape and sets to CORNOR of Image
   ellipseMode(CORNER);
   ellipse(250, 250, 100, 100);
-
+  //Colors the Shape
   fill(10, 100, 90, 255);
   ellipse(250, 250, 80, 80);
-
+  //Colors the Shape
   fill(20, 100, 1, 40);
   ellipse(250, 250, 60, 60);
-
+  //Colors the Shape
   fill(30, 200, 50, 90);
   ellipse(250, 250, 40, 40);
-
+  //Colors the Shape
   fill(30, 150, 90, 67);
   ellipse(250, 250, 20, 20);
 
