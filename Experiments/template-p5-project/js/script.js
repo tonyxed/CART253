d448a8b11be41029ac33c4e7a851f9b37f7163b6
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-08 11:54:25 -0400

    Drawing Color

--- Experiments/template-p5-project/js/script.js
+++ Experiments/template-p5-project/js/script.js
@@ -13,10 +13,28 @@ Here is a description of this template p5 project.
 function setup() {
   createCanvas(500, 500);
 
-  background(255, 138, 100);
-  rectMode(CENTER);
-  rect(250, 250, 100, 100);
-  
+  background(100, 110, 111);
+
+  //noStroke();
+  stroke(255, 100, 2);
+  fill(50, 80, 100, 120);
+
+  ellipseMode(CORNER);
+  ellipse(250, 250, 100, 100);
+
+  fill(10, 100, 90, 255);
+  ellipse(250, 250, 80, 80);
+
+  fill(20, 100, 1, 40);
+  ellipse(250, 250, 60, 60);
+
+  fill(30, 200, 50, 90);
+  ellipse(250, 250, 40, 40);
+
+  fill(30, 150, 90, 67);
+  ellipse(250, 250, 20, 20);
+
+
 }
 
 // draw()
