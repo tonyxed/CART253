636f5865f12d9adf5e503338cd7c643881305492
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-15 13:37:26 -0400

    Starting variables

---
+++ Experiments/template-p5-project-Week 2-Variables/js/script.js
@@ -0,0 +1,32 @@
+/**
+Variables
+Anthony Calderone
+
+Variables as a whole!
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
