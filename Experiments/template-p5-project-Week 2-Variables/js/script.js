855bd1df5af27a53f2be7835940a47cb1c5b0924
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-15 16:14:31 -0400

    Random Numbers

--- Experiments/template-p5-project-Week 2-Variables/js/script.js
+++ Experiments/template-p5-project-Week 2-Variables/js/script.js
@@ -14,10 +14,11 @@ function preload() {
 //Variables
 let backgroundShade = 0;
 let circle = {
-  x: 0,
+  x: 250,
   y: 250,
   size: 200,
   speed: 2,
+  fill: 0,
 };
 
 function setup() {
@@ -27,10 +28,18 @@ function setup() {
 
 function draw() {
   background(backgroundShade);
+
+  circle.speed = random(-5, 5);
   circle.x += circle.speed;
+  circle.y = random(0, height);
+  circle.size = random(10, 100);
+
+  circle.fill = random(1, 255);
+  fill(circle.fill);
+
   ellipse(circle.x, circle.y, circle.size);
 
-  console.log(`circle.x: ${circle.x} circle.y: ${circle.y} circle.size: ${circle.size} circle.speed: ${circle.speed}`);
+  console.log(`circle.fill: ${circle.fill} circle.speed: ${circle.speed} circle.y: ${circle.y} circle.size: ${circle.size}`);
 
 
 }
