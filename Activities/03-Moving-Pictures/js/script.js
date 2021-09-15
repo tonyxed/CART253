3fce9729c18e624938f2e2fb2757c3e36805a85e
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-15 19:49:49 -0400

    Exercise 2 Moving Circles Finished

--- Activities/03-Moving-Pictures/js/script.js
+++ Activities/03-Moving-Pictures/js/script.js
@@ -1,33 +1,73 @@
 /**
-Title of Project
-Author Name
+Moving Pictures
+Anthony Calderone
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+03 Moving Pictures Activity!
 */
 
 "use strict";
 
 
 /**
-Description of preload
+
 */
+
 function preload() {
-
 }
 
+//Variables
+let bgShade = 0;
 
-/**
-Description of setup
-*/
+let bg = {
+r: 1,
+g: 0,
+b: 0,
+};
+let circle1 = {
+x: 0,
+y: 250,
+size: 1,
+fill: (255,200),
+speed: 1,
+
+};
+
+let circle2 = {
+x: 500,
+y: 250,
+size: 0.9,
+fill: (255, 200),
+speed: 1,
+};
+
 function setup() {
+  createCanvas(500, 500);
+  noStroke();
 
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
+//Background
+background(bg.r, bg.g, bg.b);
+bg.r = bg.r + 1;
+
+//circle1
+circle1.x += circle1.speed;
+circle1.x = constrain(circle1.x, 0, width / 2);
+circle1.size = circle1.size + 1;
+circle1.size = constrain(circle1.size, 0, width);
+fill(circle1.fill);
+ellipse(circle1.x, circle1.y, circle1.size);
+
+//circle2
+circle2.x -= circle2.speed;
+circle2.x = constrain(circle2.x, width / 2, 500);
+circle2.size = circle2.size - 0.9;
+circle2.size = constrain(circle1.size, 0, width);
+fill(circle2.fill);
+ellipse(circle2.x, circle2.y, circle2.size);
+
+
+
 
 }
