a672fc71153e79bf180e8c45790ad3d2176fa710
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-26 21:11:54 -0400

    02: Player Movement with Arrow Keys

--- Exercises/02-dodge-em/js/script.js
+++ Exercises/02-dodge-em/js/script.js
@@ -21,7 +21,8 @@ let circleCovid = {
 let playerCircle = {
   x: 250,
   y: 250,
-  size: 100
+  size: 100,
+  speed: 5
 };
 "use strict";
 
@@ -44,14 +45,26 @@ function draw() {
   let d = dist(playerCircle.x, playerCircle.y, circleCovid.x, circleCovid.y);
   if (d < circleCovid.size / 4 + playerCircle.size / 4) {
     //noLoop();
-    }
-  playerCircle.x = mouseX;
-  playerCircle.y = mouseY;
+  }
+  //playerCircle.x = mouseX;
+  //playerCircle.y = mouseY;
 
-  //playerCircle
+
   fill(255);
-  ellipse(mouseX, mouseY, playerCircle.size);
-
+  ellipse(playerCircle.x, playerCircle.y, playerCircle.size);
+  //playerCircle Movement
+  if (keyIsDown(LEFT_ARROW)) {
+    playerCircle.x -= 5;
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    playerCircle.x += 5;
+  }
+  if (keyIsDown(UP_ARROW)) {
+    playerCircle.y -= 5;
+  }
+  if (keyIsDown(DOWN_ARROW)) {
+    playerCircle.y += 5;
+  }
   //circleCovid
   let y = random(0, height);
   noStroke();
@@ -61,12 +74,12 @@ function draw() {
   ellipse(circleCovid.x, circleCovid.y, circleCovid.size);
 
   //circleCovid follows playerCircle
-  if (mouseX < circleCovid.x) {
+  if (playerCircle.x < circleCovid.x) {
     circleCovid.ax = -circleCovid.acceleration;
   } else {
     circleCovid.ax = circleCovid.acceleration;
   }
-  if (mouseY < circleCovid.y) {
+  if (playerCircle.y < circleCovid.y) {
     circleCovid.ay = -circleCovid.acceleration;
   } else {
     circleCovid.ay = circleCovid.acceleration;
