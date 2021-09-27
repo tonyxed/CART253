d6fb1eddfd0c8b4c11a8b4224c8a1dbf6b77b1cc
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-26 20:04:04 -0400

    QOL changes

--- Exercises/02-dodge-em/js/script.js
+++ Exercises/02-dodge-em/js/script.js
@@ -19,39 +19,40 @@ let playerCircle = {
   size: 100
 };
 "use strict";
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-background(0);
+  background(0);
 
-//Static
-for(let i = 0; i < 1000; i++){
-  let numX = random(0, width);
-  let numY = random(0, height);
-  stroke(255),
-  point(numX,numY);
-}
-//Collision between circleCovid + playerCircle
-let d = dist(playerCircle.x, playerCircle.y, circleCovid.x, circleCovid.y);
-if (d < circleCovid.size/2 + playerCircle.size/2) {
-  noLoop();
-}
-playerCircle.x = mouseX;
-playerCircle.y = mouseY;
-//playerCircle
-fill(255);
-ellipse(mouseX, mouseY, playerCircle.size);
-
-//circleCovid
-let y = random(0, height);
-noStroke();
-fill(255,0,0);
-ellipse(circleCovid.x, circleCovid.y, circleCovid.size);
-  circleCovid.x += circleCovid.speed;
-if(circleCovid.x > width){
-  circleCovid.x = 0;
-  circleCovid.y = y;
-}
+  //Static
+  for (let i = 0; i < 1000; i++) {
+    let numX = random(0, width);
+    let numY = random(0, height);
+    stroke(255),
+      point(numX, numY);
   }
+  //Collision between circleCovid + playerCircle
+  let d = dist(playerCircle.x, playerCircle.y, circleCovid.x, circleCovid.y);
+  if (d < circleCovid.size / 2 + playerCircle.size / 2) {
+    noLoop();
+  }
+  playerCircle.x = mouseX;
+  playerCircle.y = mouseY;
+  //playerCircle
+  fill(255);
+  ellipse(mouseX, mouseY, playerCircle.size);
+
+  //circleCovid
+  let y = random(0, height);
+  noStroke();
+  fill(255, 0, 0);
+  ellipse(circleCovid.x, circleCovid.y, circleCovid.size);
+  circleCovid.x += circleCovid.speed;
+  if (circleCovid.x > width) {
+    circleCovid.x = 0;
+    circleCovid.y = y;
+  }
+}
