3fc6f974ffaaad3795c36913a1e52b5636b29bd3
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-30 17:45:57 -0400

    Project1: Rough Plan!

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,16 +1,26 @@
 /**
-Title of Project
+WarPong!
 Anthony Calderone
-
+----------- NEEDS TO BE FILLED OUT----------- NEEDS TO BE FILLED OUT----------- NEEDS TO BE FILLED OUT
 This is a template. You must fill in the title,
 author, and this description to match your project!
+----------- NEEDS TO BE FILLED OUT----------- NEEDS TO BE FILLED OUT----------- NEEDS TO BE FILLED OUT
+*/
+
+/**
+- AI controlled player
+- User controlled player
+- Score Sysem
+- Text defining the "game"
+- Title Screen
+- Winning Screen
+- Losing Screen
+- Time Limit
+- High Score
+- etc...
 */
 
 "use strict";
-
-function preload() {
-
-}
 
 function setup() {
 
