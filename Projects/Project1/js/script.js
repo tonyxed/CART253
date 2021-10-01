b44fd4ffae1940e3b2c910af74f27ab2e75420f9
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-09-30 23:19:39 -0400

    Project1: Plan

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,33 +1,30 @@
 /**
-Title of Project
-Author Name
+Project1
+Anthony Calderone
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+---------------------------------------------------------ADD SOMETHING HERE--------
+=-------------------------------------------------------------
+*/
+
+/**
+- Goal Posts (hitting posts will bounce off at random directions?)
+- User controlled player (using mouse to kick the "ball" at random directions?)
+- AI controlled (follows the "ball" on the x-axis trying to "save" it?)
+- Score system?
+- Background music?
+- Title Screen
+- Final Score screen?
+- An actual background?
+- The actual "ball"
+- reset system?
 */
 
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
 
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
 
 }
