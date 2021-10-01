082b73372efa5bc899f5ea68c457565133896032
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-10-01 19:06:31 -0400

    Project1: Plan 2

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,5 +1,5 @@
 /**
-Project1
+Juggling of War!
 Anthony Calderone
 
 ---------------------------------------------------------ADD SOMETHING HERE--------
@@ -7,24 +7,26 @@ Anthony Calderone
 */
 
 /**
-- Goal Posts (hitting posts will bounce off at random directions?)
-- User controlled player (using mouse to kick the "ball" at random directions?)
-- AI controlled (follows the "ball" on the x-axis trying to "save" it?)
-- Score system?
-- Background music?
-- Title Screen
-- Final Score screen?
-- An actual background?
-- The actual "ball"
+- user controlled player
+- ai controlled "balls" to be kept in the air by bouncing off of user's paddle (downward accelaration)
+- title screen
+- end screen (when no more balls are being juggled)
+- functions
+- random movement of ai controlled "balls" when hitting user paddle
+- background
+- sound of some sort
+- images of some sort
 - reset system?
+- time limit?
 */
-
 "use strict";
 
-function setup() {
 
+function setup() {
+  createCanvas(1900, 900);
 }
 
 function draw() {
+  background(0);
 
 }
