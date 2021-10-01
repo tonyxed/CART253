932f94193cbb73b113c8f5b45b3c679603e039cf
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-10-01 19:41:28 -0400

    Project1: User Controller

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,19 +1,16 @@
 /**
-Juggling of War!
 Anthony Calderone
 
----------------------------------------------------------ADD SOMETHING HERE--------
-=-------------------------------------------------------------
+Juggle of War!
 */
 
 /**
-- user controlled player
+- user controlled player (arrow keys)
 - ai controlled "balls" to be kept in the air by bouncing off of user's paddle (downward accelaration)
 - title screen
 - end screen (when no more balls are being juggled)
-- functions
 - random movement of ai controlled "balls" when hitting user paddle
-- background
+- background // Done
 - sound of some sort
 - images of some sort
 - reset system?
@@ -21,12 +18,58 @@ Anthony Calderone
 */
 "use strict";
 
+let user = {
+  x: -180,
+  y: 450,
+  w: 250,
+  h: 25,
+  size: 50,
+  speed: 5,
+  boost: 7,
+};
+let shade = {
+  r: 119,
+  g: 135,
+  b: 127,
+};
 
 function setup() {
-  createCanvas(1900, 900);
+  createCanvas(1800, 1000);
 }
 
 function draw() {
-  background(0);
+  background(shade.r, shade.g, shade.b);
+  userBasic();
+  userController();
 
+}
+
+function userBasic() {
+  //user basic
+  push();
+  stroke(1);
+  angleMode(DEGREES);
+  translate(width / 2, height / 2);
+  fill(25, 111, 181);
+  rect(user.x, user.y, user.w, user.h);
+  pop();
+}
+
+function userController() {
+  //user movement LEFT_ARROW
+  if (keyIsDown(LEFT_ARROW)) {
+    user.x -= user.speed;
+  }
+  //user movement RIGHT_ARROW
+  if (keyIsDown(RIGHT_ARROW)) {
+    user.x += user.speed;
+  }
+  // user movement speed boost moving left using UP_ARROW
+  if (keyIsDown(UP_ARROW)) {
+    user.x -= user.boost;
+  }
+  // user movement speed boost moving right using DOWN_ARROW
+  if (keyIsDown(DOWN_ARROW)) {
+    user.x += user.boost;
+  }
 }
