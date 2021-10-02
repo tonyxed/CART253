b87180e30f3ba469b2a8ec2c5cffb01948c151d0
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-10-02 11:27:28 -0400

    Project1Final: Draft + Summary

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -1,33 +1,28 @@
 /**
-Title of Project
-Author Name
+Enlarged!
+Anthony Calderone
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+You are a White Blood Cell and you need to ensure that the bacteria in your host's
+body doesn't enlarge to be able to take over essential organs, while avoiding the
+acidity essential for bacterial growth.
 */
-
+/**
+- Bacteria grows over time
+- user controlled player (white blood cell)
+- user on top of bacteria === bacteria decrease in size else bacteria keeps growing
+over time
+- Time limit?
+- Title
+- Game over
+- Winning State?
+- agent (acidity) chasing player to prevent bacteria growth
+ */
 "use strict";
 
-
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
 
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
 
 }
