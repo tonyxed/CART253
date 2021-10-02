0e13ea305901c7c9e0629ab033a65e83cad32492
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-10-02 17:41:24 -0400

    Project1: All bacteria's implemented will all their functionalities

--- Projects/Project1/js/script.js
+++ Projects/Project1/js/script.js
@@ -57,29 +57,40 @@ let bacteria1 = {
   b: 255,
   growthrate: .2,
   growthreduce: 0.5,
-  maxsize: 400,
+  maxsize: 300,
 };
 let bacteria2 = {
-  x: 1500,
-  y: 600,
+  x: 1700,
+  y: 700,
   size: 5,
   r: 255,
   g: 255,
   b: 255,
   growthrate: .1,
   growthreduce: .3,
-  maxsize: 400,
+  maxsize: 300,
 };
 let bacteria3 = {
-  x: 500,
+  x: 700,
   y: 600,
   size: 5,
   r: 255,
   g: 255,
   b: 255,
-  growthrate: .1,
-  growthreduce: 0.2,
-  maxsize: 400,
+  growthrate: .05,
+  growthreduce: .1,
+  maxsize: 300,
+};
+let bacteria4 = {
+  x: 1600,
+  y: 300,
+  size: 5,
+  r: 255,
+  g: 255,
+  b: 255,
+  growthrate: .03,
+  growthreduce: .1,
+  maxsize: 300,
 };
 
 function setup() {
@@ -91,6 +102,7 @@ function draw() {
   bacteria1Controller();
   bacteria2Controller();
   bacteria3Controller();
+  bacteria4Controller();
   userController();
   acidityController();
 }
@@ -141,6 +153,22 @@ function bacteria3Controller() {
     bacteria3.size -= bacteria3.growthreduce;
   }
   if (bacteria3.size >= bacteria3.maxsize) {
+    noLoop();
+  }
+}
+
+function bacteria4Controller() {
+  //bacteria4
+  push();
+  fill(bacteria4.r, bacteria4.g, bacteria4.b);
+  ellipse(bacteria4.x, bacteria4.y, bacteria4.size);
+  pop();
+  bacteria4.size += bacteria4.growthrate;
+  let db4 = dist(user.x, user.y, bacteria4.x, bacteria4.y);
+  if (db4 < bacteria4.size / 4 + user.size / 4) {
+    bacteria4.size -= bacteria4.growthreduce;
+  }
+  if (bacteria4.size >= bacteria4.maxsize) {
     noLoop();
   }
 }
