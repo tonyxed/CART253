d41102972d5fc55df3b3968bea7132a182bd18e6
Anthony Calderone <anthony.calderone2000@yahoo.com>
2021-11-02 14:17:19 -0400

    Prototype

--- Projects/Project2/Prototype/TemplatePrototype/js/script.js
+++ Projects/Project2/Prototype/TemplatePrototype/js/script.js
@@ -1,33 +1,36 @@
 /**
-Title of Project
-Author Name
+Final Project Protype
+Anthony Calderone
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+Final Project Prototype //
 */
 
 "use strict";
+let trucks = []
+let numTrucks = 3;
 
 
-/**
-Description of preload
-*/
-function preload() {
-
-}
-
-
-/**
-Description of setup
-*/
 function setup() {
-
+  createCanvas(500, 700);
+  //creates the trucks in the array
+  for (let i = 0; i < numTrucks; i++) {
+    let truck = new Truck(x, y, w, h);
+    let x = random(0, width);
+    let y = random(0, height);
+    let w = random(20, 40);
+    let h = random(5, 10);
+    //adds the trucks to the array
+    trucks.push(truck);
+  }
 }
 
-
-/**
-Description of draw()
-*/
 function draw() {
-
+  background(0);
+  //displays the trucks in the array
+  for (let i = 0; i < trucks.length; i++) {
+    let truck = trucks[i];
+    truck.display();
+    truck.move();
+    truck.offScreen();
+  }
 }
